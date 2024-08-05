130181d3c9dfadbef1b37228b7c7454b88604c44
Magid Ba <magid.ba@mirahtec.com>
2024-08-05 16:59:12 +0000

    delete livre

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,8 @@ Route::apiResource('livres', LivreController::class)->only('index', 'show');
 Route::middleware("auth")->group(
     function () {
         Route::get("logout", [AuthController::class, "logout"]);
-        Route::post('livres', [LivreController::class, 'store']);
+        // Route::post('livres', [LivreController::class, 'store']);
+        Route::apiResource('livres', LivreController::class)->only('store', 'destroy');
         Route::post('livres/{livre}', [LivreController::class, 'update']);
         Route::get("refresh", [AuthController::class, "refresh"]);
     }
